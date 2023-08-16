cf70a4cdbc246047bcfd6b6a1fb7b9e523264e90
efecagrisen <efecagrisen@yahoo.com>
2023-08-16 13:56:09 +0300

    lab completed

--- src/main/java/com/cydeo/model/Employee.java
+++ src/main/java/com/cydeo/model/Employee.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
@@ -15,7 +16,10 @@ public class Employee {
 
     private String firstName;
     private String lastName;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd") //thymeleaf accepts this format so, we need to tell LocalDate to format itself to avoid confusion
     private LocalDate birthday;
+
     private String email;
     private String password;
     private String address;
